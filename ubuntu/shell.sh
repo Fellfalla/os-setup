21a252a54d50c3154e9270d59c60cf8a51c0feae
Markus Weber <weber@filics.eu>
2023-01-05 18:53:02 +0100

    add powerline font

--- ubuntu/shell.sh
+++ ubuntu/shell.sh
@@ -6,10 +6,13 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 # fonts
 sudo apt install fonts-firacode -y
+sudo apt install fonts-powerline -y
 
 # autofill
+# TODO: make this work when installinf zsh for the first time
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+# TODO: find and replace the plugin section in .zshrc or overwrite using a default
 echo """
 Add the plugin to the list of plugins for Oh My Zsh to load (inside ~/.zshrc):
 
