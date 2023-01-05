9d3e274613b80e11dcd92758d3268425f07d6a07
Markus Weber <weber@filics.eu>
2023-01-05 19:39:52 +0100

    workaround: install docker-compose if docker group already exsits

--- ubuntu/docker.sh
+++ ubuntu/docker.sh
@@ -20,15 +20,16 @@ echo \
 sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 
-# Make docker executable without super privileges
-# TODO: continue script if docker group already exists
-sudo groupadd docker # 1. create `docker` group (if not already created)
-sudo usermod -aG docker $USER # 2. add user to `docker` group
-newgrp docker # 3. log out and log back in, or reboot. Verify by running `docker run hello-world`
-
-
 ##### Install Install docker-compose #####
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose # Download the docker-compose software
 sudo chmod +x /usr/local/bin/docker-compose # Make it executable
 sudo apt remove docker-compose # Make sure you have uninstalley previous versions of docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose # Link the binary to the default bin path
+
+
+##### Make docker executable without super privileges #####
+# TODO: continue script if docker group already exists
+sudo groupadd docker # 1. create `docker` group (if not already created)
+sudo usermod -aG docker $USER # 2. add user to `docker` group
+newgrp docker # 3. log out and log back in, or reboot. Verify by running `docker run hello-world`
+
