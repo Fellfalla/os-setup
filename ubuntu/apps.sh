e84f7ea096c064f1eabd4895db53ff150db61a8b
Markus Weber <weber@filics.eu>
2023-01-05 18:52:54 +0100

    sudo snap install to avoid prompts

--- ubuntu/apps.sh
+++ ubuntu/apps.sh
@@ -12,7 +12,7 @@ sudo apt install git -y
 sudo apt install npm -y
 sudo apt install python3-pip -y
 
-snap install code --classic # IDEs for coding
+sudo snap install code --classic # IDEs for coding
 # Install all vscode extensions
 declare -a VSCODE_EXTENSIONS=(
     "ms-vscode.cpptools"
@@ -39,14 +39,14 @@ sudo apt install terminator -y
 
 ##### Productive Tools #####
 # TODO: install firefox addons https://askubuntu.com/questions/73474/how-to-install-firefox-addon-from-command-line-in-scripts
-snap install notion-snap
-snap install teams-for-linux
+sudo snap install notion-snap
+sudo snap install teams-for-linux
 
 
 ##### Security #####
-snap install bitwarden # Password manager
+sudo snap install bitwarden # Password manager
 
 
 ##### Fun Corner #####
-sudo apt install steam
-snap install spotify
+sudo apt install steam -y
+sudo snap install spotify
